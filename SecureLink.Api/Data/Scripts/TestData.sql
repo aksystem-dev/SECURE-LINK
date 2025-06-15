283b5e0b581134ef5c2314e46d77dcaec04c1d4e
Tlapy13 <102100865+Tlapy13@users.noreply.github.com>
2025-06-15 19:42:31 +0200

    Update TestData.sql
    test data update

--- SecureLink.Api/Data/Scripts/TestData.sql
+++ SecureLink.Api/Data/Scripts/TestData.sql
@@ -1,21 +1,7 @@
-﻿USE [SecureLink];
+USE [SecureLink];
 GO
 
--- 1) Vložení dat do tabulky UserTypes
-INSERT INTO [dbo].[UserTypes] ([Name], [Description])
-VALUES 
-  ('Admin', 'Full access to the system'),
-  ('Guest', 'Limited access'),
-  ('Reader', 'Read-only access'),
-  ('Writer', 'Write and read access');
-GO
-
--- 2) Vložení uživatele do tabulky Users => heslo: test
-  insert into Users (Username, PasswordHash, UserType, CreatedAt, DatabaseName)
-  values ('EmailSMSGate', '$2a$11$N0LQB/FDkBSubBAA0alX6ehs0R1IhCB9wE81d.4TWvc.b8KshQg9q', 1, GETDATE(), 'DefaultConnection')
-GO
-
--- 3) Vložení dummy dat do tabulky SecureLinkSettings
+-- Vložení dummy dat do tabulky SecureLinkSettings
 INSERT INTO [dbo].[SecureLinkSettings] ([EncryptedKey], [Message], [ExpirationDate], [DatabaseName])
 VALUES 
   ('dummyEncryptedKey1', 'This is a secure link message.', DATEADD(DAY, 30, GETDATE()), 'DefaultConnection');
